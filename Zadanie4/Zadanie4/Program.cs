363cf510fc4bb70d93c965555b684f096b9c665e
Lenovo <Lenovo@DESKTOP-JSF0E5Q>
2023-09-29 21:35:41 +0200

    Zadanie 4 od:  Andrzej (ASKA)

---
+++ Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,19 @@
+﻿// ASKA Zadanie4
+
+var myMame = "Ewa";
+var mySex = "Kobieta";
+var myAge = 33;
+
+if (mySex == "Kobieta" && myAge == 33)
+{
+    Console.WriteLine("Ewa lat 33.");
+}
+else if (mySex == "Kobieta" && myAge < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (mySex != "Kobieta" && myAge < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
+
